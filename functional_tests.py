f9831d8c5eecef01c753e3af060faa9ffba31af1
Patrick Lahey <code.grasshopper@gmail.com>
2015-08-28 15:26:04 -0700

    Added POST of new to-do items are toy response of last item entered

--- functional_tests.py
+++ functional_tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                inputbox.get_attribute('placehold'),
+                inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
 
@@ -38,15 +38,22 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-                any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # there is still a text box inviting her to add another item.  She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 
         # The page updates again, and now shows both items on her list
+
+        self.fail('Finish the test!')
 
         # Edith wonders whether the site will remember her list.  Then she sees
         # that the site has generated a unique URL for her -- there is some
